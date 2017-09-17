9342ee52276ddcb7542eeb8246345c946e6b7dd3
jaeyoung.choi <lacti@linecorp.com>
2017-09-17 14:24:05 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        enforce: 'pre',
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        loader: 'source-map-loader',
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devtool: 'inline-source-map'
+}
