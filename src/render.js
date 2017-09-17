d787c52e17686b901f6a1a368fe492b0311bc4bc
jaeyoung.choi <lacti@linecorp.com>
2017-09-17 19:50:51 +0900

    make Renderer as a module

--- src/render.js
+++ src/render.js
@@ -7,45 +7,13 @@ const r = {
     move: 400
   }
 }
-
-const canvas = document.getElementById('canvas')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-
-const ctx = canvas.getContext('2d')
-console.log(ctx)
-
-const drawers = {
-  fillBackground: (color) => {
-    ctx.fillStyle = color
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-  }
-}
-const loadResource = (path, afterLoaded) => {
-  const resources = document.getElementById('resources')
-  const img = document.createElement('img')
-  img.src = path
-  img.addEventListener('load', () => afterLoaded(img))
-  resources.appendChild(img)
-}
-
-const loadResources = (afterLoaded) => {
-  loadResource('images/car.png', (img) => {
-    drawers.drawCar = (index, x, y, sprite) => {
-      ctx.drawImage(img, r.car.w * index, sprite * r.car.h, r.car.w, r.car.h, x, y, r.car.w, r.car.h)
-    }
-    afterLoaded()
-  })
-}
-
 class Camera {
-  me(me) {
-    this.me = me
-  }
   update() {
-    const pos = this.me.pos();
-    this.x = pos.x
-    this.y = pos.y
+    if (this.center) {
+      const pos = this.center.pos();
+      this.x = pos.x
+      this.y = pos.y
+    }
   }
   translate(pos) {
     return {
@@ -54,7 +22,6 @@ class Camera {
     }
   }
 }
-const camera = new Camera()
 
 class TimeGauge {
   constructor(interval, chooses) {
@@ -71,11 +38,13 @@ class TimeGauge {
     return this.chooses[this.index]
   }
 }
+
 class CarRO {
-  constructor(index, initPos) {
+  constructor(renderer, index, initPos) {
+    this.renderer = renderer
     this.index = index
     this.from = this.to = (initPos || { x: 0, y: 0 })
-    this.sprite = new TimeGauge(128, [0, 1]);
+    this.sprite = new TimeGauge(128, [0, 1])
   }
   update(dt) {
     let x = this.to.x * r.car.w
@@ -102,35 +71,82 @@ class CarRO {
     return { x: this.x, y: this.y }
   }
   draw(dt) {
-    let pos = camera.translate(this.pos())
-    drawers.drawCar(this.index, pos.x, pos.y, this.sprite.update(dt))
+    let pos = this.renderer.camera.translate(this.pos())
+    renderer.drawers.drawCar(this.index, pos.x, pos.y, this.sprite.update(dt))
   }
 }
 
-const cars = []
-const car = new CarRO(0)
-const car2 = new CarRO(1, { x: 1, y: 0 })
+export default class Renderer {
+  constructor() {
+    this.canvas = document.getElementById('canvas')
+    this.canvas.width = window.innerWidth
+    this.canvas.height = window.innerHeight
 
-cars.push(car)
-cars.push(car2)
+    this.ctx = canvas.getContext('2d')
+    this.drawers = {
+      fillBackground: (color) => {
+        this.ctx.fillStyle = color
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+      }
+    }
 
-camera.me(car)
-
-let y = canvas.height - 50
-const render = (dt) => {
-  drawers.fillBackground('#888888')
-  cars.forEach(each => each.update(dt))
-  camera.update()
-  cars.forEach(each => each.draw(dt))
-}
-
-loadResources(() => {
-  let lastRender = 0
-  const renderLoop = (timestamp) => {
-    const delta = timestamp - (lastRender || timestamp)
-    render(delta)
-    lastRender = timestamp
-    window.requestAnimationFrame(renderLoop)
+    this.camera = new Camera()
+    this.cars = []
   }
-  window.requestAnimationFrame(renderLoop)
-});
+
+  spawn(index, pos) {
+    const car = new CarRO(this, index, pos)
+    if (!this.camera.center) {
+      this.camera.center = car
+    }
+    this.cars.push(car)
+    return car
+  }
+
+  loadResource(path, afterLoaded) {
+    const resources = document.getElementById('resources')
+    const img = document.createElement('img')
+    img.src = path
+    img.addEventListener('load', () => afterLoaded(img))
+    resources.appendChild(img)
+  }
+
+  loadResources(afterLoaded) {
+    this.loadResource('images/car.png', (img) => {
+      this.drawers.drawCar = (index, x, y, sprite) => {
+        this.ctx.drawImage(img, r.car.w * index, sprite * r.car.h, r.car.w, r.car.h, x, y, r.car.w, r.car.h)
+      }
+      afterLoaded()
+    })
+  }
+
+  render(dt) {
+    this.drawers.fillBackground('#888888')
+    this.cars.forEach(each => each.update(dt))
+    this.camera.update()
+    this.cars.forEach(each => each.draw(dt))
+  }
+
+  start() {
+    this.loadResources(() => {
+      let lastRender = 0
+      const renderLoop = (timestamp) => {
+        const delta = timestamp - (lastRender || timestamp)
+        this.render(delta)
+        lastRender = timestamp
+        window.requestAnimationFrame(renderLoop)
+      }
+      window.requestAnimationFrame(renderLoop)
+    });
+  }
+
+  demo() {
+    const car = this.spawn(0, { x: 0, y: 0 })
+    this.spawn(1, { x: 1, y: 0 })
+
+    let posY = 0
+    setInterval(() => {
+      car.move({ x: 0, y: posY++ })
+    }, 1000)
+  }
+}
