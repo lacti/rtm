1422e90b73d1b6904f2ec77a438c133404b2ef32
jaeyoung.choi <lacti@linecorp.com>
2017-09-17 17:54:38 +0900

    add sample rendering code.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-export default class Rtm {
+class Rtm {
 
+  constructor() {
+  }
+}
+
+const rtm = new Rtm();
+
+const canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+console.log('hello!');
+
+const ctx = canvas.getContext("2d");
+console.log(ctx);
+if (ctx) {
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(0, 0, 150, 75);
 }
